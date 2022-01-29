84d1c14c85dd74c7d3c02af42947c48fa82952d1
Padr02 <pavel.drepalov@gmail.com>
2022-01-29 21:15:51 +0100

    AccountServiceUsersTest OK/ Hela laget

--- src/main/java/se/sensera/banking/services/AccountServiceImpl.java
+++ src/main/java/se/sensera/banking/services/AccountServiceImpl.java
@@ -2,11 +2,10 @@ package se.sensera.banking.services;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import se.sensera.banking.Account;
-import se.sensera.banking.AccountService;
-import se.sensera.banking.AccountsRepository;
-import se.sensera.banking.UsersRepository;
+import se.sensera.banking.*;
+import se.sensera.banking.exceptions.Activity;
 import se.sensera.banking.exceptions.UseException;
+import se.sensera.banking.exceptions.UseExceptionType;
 
 import java.util.function.Consumer;
 import java.util.stream.Stream;
@@ -30,12 +29,52 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public Account addUserToAccount(String userId, String accountId, String userIdToBeAssigned) throws UseException {
-        return null;
+        User user = usersRepository.getEntityById(userId).get();
+        User user1 = usersRepository.getEntityById(userIdToBeAssigned).get();
+        if  (accountsRepository.getEntityById(accountId).isEmpty()){
+            throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.NOT_FOUND);
+        }
+        Account account = accountsRepository.getEntityById(accountId).get();
+
+        if (!account.isActive() ) {
+            throw new UseException(Activity.UPDATE_ACCOUNT, UseExceptionType.ACCOUNT_NOT_ACTIVE);
+        }
+
+        if (account.getOwner().equals(user1)){
+            throw new UseException(Activity.UPDATE_ACCOUNT,UseExceptionType.CANNOT_ADD_OWNER_AS_USER);
+        }
+
+        if (!account.getOwner().getId().equals(user.getId())){
+            throw new UseException(Activity.UPDATE_ACCOUNT,UseExceptionType.NOT_OWNER);
+        }
+
+        if (account.getUsers().anyMatch(user2 -> user2.getId().equals(userIdToBeAssigned))){
+            throw new UseException(Activity.UPDATE_ACCOUNT,UseExceptionType.USER_ALREADY_ASSIGNED_TO_THIS_ACCOUNT);
+        }
+
+        account.addUser(user1);
+        accountsRepository.save(account);
+
+        return account;
     }
 
     @Override
     public Account removeUserFromAccount(String userId, String accountId, String userIdToBeAssigned) throws UseException {
-        return null;
+        Account account=accountsRepository.getEntityById(accountId).get();
+        User user=usersRepository.getEntityById(userId).get();
+        User userToBeAssigned=usersRepository.getEntityById(userIdToBeAssigned).get();
+
+        if (!account.getOwner().getId().equals(user.getId())){
+            throw new UseException(Activity.UPDATE_ACCOUNT,UseExceptionType.NOT_OWNER);
+        }
+
+        if (account.getUsers().noneMatch(user2 -> user2.getId().equals(userIdToBeAssigned))){
+            throw new UseException(Activity.UPDATE_ACCOUNT,UseExceptionType.USER_NOT_ASSIGNED_TO_THIS_ACCOUNT);
+        }
+
+        account.removeUser(userToBeAssigned);
+        accountsRepository.save(account);
+        return account;
     }
 
     @Override
