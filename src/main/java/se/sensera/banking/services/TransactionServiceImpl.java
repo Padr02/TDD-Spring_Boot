74b34419a56911c13772d6c3ac6d5deff4b5963b
Padr02 <pavel.drepalov@gmail.com>
2022-02-07 19:35:10 +0100

    Refactored TransactionServiceImpl

--- src/main/java/se/sensera/banking/services/TransactionServiceImpl.java
+++ src/main/java/se/sensera/banking/services/TransactionServiceImpl.java
@@ -1,10 +1,12 @@
 package se.sensera.banking.services;
+
 import lombok.Data;
 import se.sensera.banking.*;
 import se.sensera.banking.exceptions.Activity;
 import se.sensera.banking.exceptions.UseException;
 import se.sensera.banking.exceptions.UseExceptionType;
 import se.sensera.banking.implementeringar.TransactionImpl;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -29,60 +31,61 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public Transaction createTransaction(String created, String userId, String accountId, double amount) throws UseException {
-
         getDate(created);
         Account account = getAccount(accountId);
         User user = getUser(userId);
-
-        double sum = transactionsRepository.all()
-                .filter(x -> x.getAccount()
-                        .equals(account))
-                .mapToDouble(Transaction::getAmount)
-                .sum();
-
-        if ((amount + sum) < 0) {
-            throw new UseException(Activity.CREATE_TRANSACTION, UseExceptionType.NOT_FUNDED);
-        }
-
-        if (account.getUsers().anyMatch(u -> u.equals(user))) {
-
-            return transactionsRepository.save(new TransactionImpl(UUID.randomUUID().toString(),date, user, account, amount));
-        }
-
-        if (!account.getOwner().equals(user)) {
-            throw new UseException(Activity.CREATE_TRANSACTION, UseExceptionType.NOT_ALLOWED);
-        }
-
-        var transaction = new TransactionImpl(UUID.randomUUID().toString(),date, user, account, amount);
-
-        synchronized (transaction) {
-            consumerList.forEach(tra -> tra.accept(transaction));
-        }
+        double sum = getSumCurrentDate(account);
+        isFunded(amount, sum);
+        isTransactionAllowed(account, user);
+        var transaction = new TransactionImpl(UUID.randomUUID().toString(), date, user, account, amount);
+        consumerList.forEach(tra -> tra.accept(transaction));
 
         return transactionsRepository.save(transaction);
     }
 
+    private void isFunded(double amount, double sum) throws UseException {
+        if ((amount + sum) < 0) {
+            throw new UseException(Activity.CREATE_TRANSACTION, UseExceptionType.NOT_FUNDED);
+        }
+    }
+
+    private double getSumCurrentDate(Account account) {
+        return transactionsRepository.all()
+                .filter(x -> x.getAccount()
+                        .equals(account))
+                .mapToDouble(Transaction::getAmount)
+                .sum();
+    }
+
+    private void isTransactionAllowed(Account account, User user) throws UseException {
+        if (!account.getOwner().equals(user) && account.getUsers().noneMatch(u -> u.equals(user))) {
+            throw new UseException(Activity.CREATE_TRANSACTION, UseExceptionType.NOT_ALLOWED);
+        }
+    }
+
     @Override
     public double sum(String created, String userId, String accountId) throws UseException {
-
         getDate(created);
-
         Account account = getAccount(accountId);
         User user = getUser(userId);
+        isSumAllowed(account, user);
+        Date otherDate = date;
+        return getSumBeforeDate(account, otherDate);
+    }
 
-        if (account.getOwner().getId().equals(user.getId()) || account.getUsers().anyMatch(x -> x.equals(user))) {
+    private double getSumBeforeDate(Account account, Date otherDate) {
+        return transactionsRepository.all()
+                .filter(x -> x.getAccount()
+                        .equals(account))
+                .filter(u -> u.getCreated().before(otherDate))
+                .mapToDouble(Transaction::getAmount)
+                .sum();
+    }
 
-            Date date1 = date;
-
-            return transactionsRepository.all()
-                    .filter(x -> x.getAccount()
-                            .equals(account))
-                    .filter(u -> u.getCreated().before(date1))
-                    .mapToDouble(Transaction::getAmount)
-                    .sum();
-
+    private void isSumAllowed(Account account, User user) throws UseException {
+        if (!account.getOwner().getId().equals(user.getId()) && account.getUsers().noneMatch(x -> x.equals(user))) {
+            throw new UseException(Activity.SUM_TRANSACTION, UseExceptionType.NOT_ALLOWED);
         }
-        throw new UseException(Activity.SUM_TRANSACTION, UseExceptionType.NOT_ALLOWED);
     }
 
     private void getDate(String created) {
