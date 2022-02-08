56750ba6d22454ebc31556302ce940fbfd22ba7d
Padr02 <pavel.drepalov@gmail.com>
2022-02-08 10:45:20 +0100

    Refactored UserService

--- src/main/java/se/sensera/banking/services/UserServiceImpl.java
+++ src/main/java/se/sensera/banking/services/UserServiceImpl.java
@@ -11,53 +11,45 @@ import se.sensera.banking.exceptions.UseExceptionType;
 import se.sensera.banking.implementeringar.UserImpl;
 import se.sensera.banking.utils.ListUtils;
 
-import java.util.Comparator;
-import java.util.Optional;
-import java.util.UUID;
+
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+
 @Data
 @AllArgsConstructor
+
 public class UserServiceImpl implements UserService {
 
     UsersRepository usersRepository;
 
     @Override
-    public User createUser(String name, String personalIdentificationNumber) throws UseException {
+    public User createUser(String name, String pId) throws UseException {
         if (usersRepository.all()
-                .anyMatch(user -> user.getPersonalIdentificationNumber().equals(personalIdentificationNumber)))
+                .anyMatch(user -> user.getPersonalIdentificationNumber().equals(pId)))
             throw new UseException(Activity.CREATE_USER, UseExceptionType.USER_PERSONAL_ID_NOT_UNIQUE);
-
-        UserImpl user = new UserImpl(UUID.randomUUID().toString(), name, personalIdentificationNumber, true);
-        return usersRepository.save(user);
+        return usersRepository.save(new UserImpl(UUID.randomUUID().toString(), name, pId, true));
     }
+
 
     @Override
     public User changeUser(String userId, Consumer<ChangeUser> changeUser) throws UseException {
-        if (usersRepository.getEntityById(userId).isEmpty()) {
-            throw new UseException(Activity.UPDATE_USER, UseExceptionType.NOT_FOUND);
-        }
-
-        User user = usersRepository.getEntityById(userId).get();
-
+        User user = userExists(usersRepository, userId);
         changeUser.accept(new ChangeUser() {
             @Override
             public void setName(String name) {
                 user.setName(name);
                 usersRepository.save(user);
             }
-
             @Override
             public void setPersonalIdentificationNumber(String personalIdentificationNumber) throws UseException {
                 boolean notUnique = usersRepository.all()
                         .map(User::getPersonalIdentificationNumber)
                         .anyMatch(personalIdentificationNumber::equals);
-
                 if (notUnique) {
                     throw new UseException(Activity.UPDATE_USER, UseExceptionType.USER_PERSONAL_ID_NOT_UNIQUE);
                 }
-
                 user.setPersonalIdentificationNumber(personalIdentificationNumber);
                 usersRepository.save(user);
             }
@@ -67,13 +59,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User inactivateUser(String userId) throws UseException {
-        User user = usersRepository
-                .getEntityById(userId)
-                .filter(User::isActive)
-                .orElseThrow(() -> new UseException(Activity.UPDATE_USER, UseExceptionType.NOT_FOUND));
-
+        User user = userExists(usersRepository, userId);
         user.setActive(false);
         return usersRepository.save(user);
+    }
+    private User userExists(UsersRepository usersRepository, String userId) throws UseException {
+        return usersRepository.getEntityById(userId)
+                .orElseThrow(() -> new UseException(Activity.UPDATE_USER, UseExceptionType.NOT_FOUND));
     }
 
     @Override
@@ -84,11 +76,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public Stream<User> find(String searchString, Integer pageNumber, Integer pageSize, SortOrder sortOrder) {
         String nameUpper = searchString.toUpperCase();
-
         return switch (sortOrder) {
             case None -> searchString.isEmpty()
                     ? ListUtils.applyPage(usersRepository.all(), pageNumber, pageSize).filter(User::isActive)
                     : usersRepository.all().filter(user -> user.getName().toUpperCase().contains(nameUpper));
+
             case Name -> usersRepository.all().sorted(Comparator.comparing(User::getName));
             case PersonalId -> usersRepository.all().sorted(Comparator.comparing(User::getPersonalIdentificationNumber));
 
